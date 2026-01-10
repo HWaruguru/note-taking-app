34697a2b3799663f0337044591ccc5adbe1c188c
njorogewaruguru <warugurunjoroge8@gmail.com>
2026-01-10 14:47:49 +0300

    set up routes

--- server/server.js
+++ server/server.js
@@ -4,9 +4,21 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-    res.send("You have 5 notes");
+    res.status(200).send("You have 5 notes");
+})
+
+app.post("/api/notes", (req, res) =>{
+    res.status(201).json({message: "Post created successfully"})
+})
+
+app.put("/api/notes/:id", (req, res)=>{
+    res.status(200).json({message: "Post updated successfully"})
+})
+
+app.delete("/api/notes/:id", (req, res)=>{
+res.status(200).json({message: "Post deleted successfully"})
 })
 
 app.listen(5001, () => {
-    console.log("Server started on PORT: 5001")
+    console.log("Server updated on PORT: 5001")
 })
