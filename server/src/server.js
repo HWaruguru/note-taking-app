5d8e26aeb51a5d240f1385f7d3a7ed06d08fb13d
njorogewaruguru <warugurunjoroge8@gmail.com>
2026-01-11 10:28:34 +0300

    optimize file and folder structure

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js"
+// const express = require("express")
+const app = express();
+
+app.use("/api/notes", notesRoutes)
+
+app.listen(5001, () => {
+    console.log("Server updated on PORT: 5001")
+})
